695660def032553ce601bcef7d3c0784d2a01610
Carson Jones <carson.jones@madmensoftware.com>
2017-05-14 02:57:30 -0500

    linked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  database: 'mongodb://heroku_w8xprj9n:heroku_w8xprj9n@ds139801.mlab.com:39801/heroku_w8xprj9n',
   secret: 'holy-cow-ballz'
 }
